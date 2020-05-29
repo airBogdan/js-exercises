072e20cd331884e01fbb0d0d692669e65887fb0d
airbogdan <bgdmhl333@gmail.com>
2020-05-29 18:52:46 +0300

    Update callbacks.js

--- js-hard-parts/callbacks.js
+++ js-hard-parts/callbacks.js
@@ -332,7 +332,7 @@ function highestFunc(objOfFuncs, subject) {
 
 // Challenge 22
 // Create a function, 'combineOperations', that takes two parameters: a starting value and an array of functions.
-// combineOperations should pass the starting value into the first function in the array. combineOperations should pass the value returned by the first function into the second function, 
+// combineOperations should pass the starting value into the first function in the array. combineOperations should pass the value returned by the first function into the second function,
 // and so on until every function in the array has been called. combineOperations should return the final value returned by the last function in the array.
 function combineOperations(startVal, arrOfFuncs) {
     for (let func of arrOfFuncs) {
